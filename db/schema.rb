e1a52e0cd511c763d98a30112baac01b02bd3aa9
JRUA <jwazzt@gmail.com>
2016-05-21 13:12:07 +0000

    added user article associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160517220245) do
+ActiveRecord::Schema.define(version: 20160521124017) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username",               default: "",    null: false
